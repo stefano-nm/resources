6d568e6230380dac08227d0d9f013dca8e5db319
Stefano Negri Merlo <s315219@studenti.polito.it>
2023-03-12 19:36:21 +0100

    failsafe service registering

--- resources/utils/service.py
+++ resources/utils/service.py
@@ -1,4 +1,23 @@
+from threading import Thread
+from typing import Callable, ParamSpec
+
 import requests
+from requests import Response
+
+_P = ParamSpec("_P")
+
+
+def to_thread(method: Callable[_P, Response]):
+    def decorator(*args: _P.args, **kwargs: _P.kwargs):
+        def _thread():
+            status = -1
+            while status != 200:
+                response = method(*args, **kwargs)
+                status = response.status_code
+
+        Thread(target=_thread, daemon=True).start()
+
+    return decorator
 
 
 class Service:
@@ -9,24 +28,23 @@ class Service:
             catalog: str,
             token: str = None
     ):
-        if address is None:
-            return
         self._service_name = name
         self._service_catalog = catalog
         self._service_address = address
         self._service_token = token
         self.service_register()
 
+    @to_thread
     def service_register(self):
-        response = requests.post(f"{self._service_catalog}/register", params={
+        return requests.post(f"{self._service_catalog}/register", params={
             "name": self._service_name,
             "endpoint": self._service_address,
             "token": self._service_token
         })
-        pass
 
+    @to_thread
     def service_unregister(self):
-        requests.delete(f"{self._service_catalog}/unregister", params={
+        return requests.delete(f"{self._service_catalog}/unregister", params={
             "name": self._service_name,
             "token": self._service_token
         })
